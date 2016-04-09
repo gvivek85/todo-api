422eb3c912bb1e8041d435719800fc24c2198fee
Vivek Gupta <vivek.gupta85@gmail.com>
2016-04-09 16:53:05 -0500

    Added PUT method

--- server.js
+++ server.js
@@ -25,6 +25,7 @@ apps.get('/todos/:id', function(req, res){
 		res.status(404).send();
 });
 
+//Post request to Add
 apps.post('/todos', function(req, res){
 	var body = _.pick(req.body, 'description', 'completed');		
 	
@@ -38,6 +39,7 @@ apps.post('/todos', function(req, res){
 	res.send(body);
 });
 
+//Delete Request
 apps.delete('/todos/:id',function(req, res){
 	var todoId = parseInt(req.params.id, 10);
 	var matchedToDo = _.findWhere(todos, {id: todoId});	
@@ -47,6 +49,35 @@ apps.delete('/todos/:id',function(req, res){
 	
 	todos = _.without(todos, matchedToDo);	
 	res.json(todos);
+});
+
+//Update Request
+apps.put('/todos/:id', function(req, res){
+	var todoId = parseInt(req.params.id, 10);
+	var matchedToDo = _.findWhere(todos, {id: todoId});
+	var body = _.pick(req.body, 'description', 'completed');	
+	var validAttributes ={};
+	
+	if(!matchedToDo)
+		return res.status(404).send();
+	
+	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+		validAttributes.completed = body.completed;
+	} else if(body.hasOwnProperty('completed')){
+		return res.status(400).send();
+	} else {
+		
+	}
+	
+	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
+		validAttributes.description = body.description;
+	} else if(body.hasOwnProperty('description')){
+		return res.status(400).send();
+	}
+	
+	_.extend(matchedToDo, validAttributes);
+	
+	res.send(todos);
 });
 
 apps.get('/', function(req, res){
