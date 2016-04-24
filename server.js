8494f89cd6a328229914a1aced18b69c3234e788
Vivek Gupta <vivek.gupta85@gmail.com>
2016-04-24 17:54:49 -0500

    Added Sequelize Update method

--- server.js
+++ server.js
@@ -78,30 +78,29 @@ apps.delete('/todos/:id',function(req, res){
 //Update Request
 apps.put('/todos/:id', function(req, res){
 	var todoId = parseInt(req.params.id, 10);
-	var matchedToDo = _.findWhere(todos, {id: todoId});
 	var body = _.pick(req.body, 'description', 'completed');	
-	var validAttributes ={};
+	var attributes ={};
 	
-	if(!matchedToDo)
-		return res.status(404).send();
+	if(body.hasOwnProperty('completed')){
+		attributes.completed = body.completed;
+	} 
 	
-	if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
-		validAttributes.completed = body.completed;
-	} else if(body.hasOwnProperty('completed')){
-		return res.status(400).send();
-	} else {
-		
-	}
-	
-	if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.trim().length > 0){
-		validAttributes.description = body.description;
-	} else if(body.hasOwnProperty('description')){
-		return res.status(400).send();
-	}
-	
-	_.extend(matchedToDo, validAttributes);
-	
-	res.send(todos);
+	if(body.hasOwnProperty('description')){
+		attributes.description = body.description;
+	} 	
+	db.todo.findById(todoId).then(function(todo){
+		if(todo){
+			todo.update(attributes).then(function(todo){
+					res.json(todo.toJSON());
+				}, function(e){
+					res.status(400).send(e);
+				});
+		} else {
+			res.status(404).send();
+		}
+	}, function(){
+		res.status(500).send();
+	});
 });
 
 db.sequelize.sync().then(function(){
